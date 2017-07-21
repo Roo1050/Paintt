94019ec1a6b63b1364de70ee996c647054c1c10b
CCLC <cclc@lr.k12.nj.us>
2017-07-21 10:00:46 -0400

    Draw app.

---
+++ paintt/paintt/Form1.Designer.vb
@@ -0,0 +1,386 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Red = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Orange = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(920, 547)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(975, 547)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(849, 228)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(849, 241)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Silver
+        Me.Button1.Location = New System.Drawing.Point(801, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 28)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Snowman"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Silver
+        Me.Button2.Location = New System.Drawing.Point(801, 46)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(74, 28)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Aquamarine"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Silver
+        Me.Button3.Location = New System.Drawing.Point(801, 80)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(74, 28)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Black"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Silver
+        Me.Button4.Location = New System.Drawing.Point(801, 495)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(74, 28)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "teal"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Silver
+        Me.Button5.Location = New System.Drawing.Point(801, 359)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(74, 28)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "pink"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Red
+        '
+        Me.Red.BackColor = System.Drawing.Color.Silver
+        Me.Red.Location = New System.Drawing.Point(801, 148)
+        Me.Red.Name = "Red"
+        Me.Red.Size = New System.Drawing.Size(74, 28)
+        Me.Red.TabIndex = 9
+        Me.Red.Text = "Red"
+        Me.Red.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.Silver
+        Me.Button7.Location = New System.Drawing.Point(801, 325)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(74, 28)
+        Me.Button7.TabIndex = 10
+        Me.Button7.Text = "purple/violet"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.Silver
+        Me.Button8.Location = New System.Drawing.Point(801, 257)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(74, 28)
+        Me.Button8.TabIndex = 11
+        Me.Button8.Text = "Green"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Silver
+        Me.Button9.Location = New System.Drawing.Point(801, 220)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(74, 28)
+        Me.Button9.TabIndex = 12
+        Me.Button9.Text = "Yellow"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Orange
+        '
+        Me.Orange.BackColor = System.Drawing.Color.Silver
+        Me.Orange.Location = New System.Drawing.Point(801, 182)
+        Me.Orange.Name = "Orange"
+        Me.Orange.Size = New System.Drawing.Size(74, 28)
+        Me.Orange.TabIndex = 13
+        Me.Orange.Text = "Orange"
+        Me.Orange.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.Silver
+        Me.Button11.Location = New System.Drawing.Point(801, 114)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(74, 28)
+        Me.Button11.TabIndex = 14
+        Me.Button11.Text = "Alice Blue"
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.Silver
+        Me.Button6.Location = New System.Drawing.Point(801, 291)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(74, 28)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "blue"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.Silver
+        Me.Button12.Location = New System.Drawing.Point(1006, 151)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(134, 59)
+        Me.Button12.TabIndex = 16
+        Me.Button12.Text = "ERASE"
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.Silver
+        Me.Button13.Location = New System.Drawing.Point(1006, 70)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(134, 60)
+        Me.Button13.TabIndex = 17
+        Me.Button13.Text = "CLEAR"
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.Silver
+        Me.Button14.Location = New System.Drawing.Point(1006, 241)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(134, 62)
+        Me.Button14.TabIndex = 18
+        Me.Button14.Text = "lol simar said "
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.Silver
+        Me.Button15.Location = New System.Drawing.Point(801, 461)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(74, 28)
+        Me.Button15.TabIndex = 19
+        Me.Button15.Text = "tan"
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.Silver
+        Me.Button10.Location = New System.Drawing.Point(801, 393)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(74, 28)
+        Me.Button10.TabIndex = 20
+        Me.Button10.Text = "light cyan"
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.Silver
+        Me.Button16.Location = New System.Drawing.Point(801, 427)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(74, 28)
+        Me.Button16.TabIndex = 21
+        Me.Button16.Text = "lavender"
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(1025, 424)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(115, 35)
+        Me.Button17.TabIndex = 22
+        Me.Button17.Text = "Thick"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(1025, 465)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(115, 35)
+        Me.Button19.TabIndex = 24
+        Me.Button19.Text = "Less Thick mr simar"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(1025, 509)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(115, 20)
+        Me.TextBox1.TabIndex = 25
+        Me.TextBox1.Text = "Insert Text Size"
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(59, 496)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(128, 45)
+        Me.Button18.TabIndex = 26
+        Me.Button18.Text = "Ultra Thick"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 547)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(228, 20)
+        Me.TextBox2.TabIndex = 27
+        Me.TextBox2.Text = "Warning: If you click again, it doubles the size."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1267, 569)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Orange)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Red)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Red As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Orange As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button18 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ paintt/paintt/Form1.vb
@@ -0,0 +1,156 @@
+﻿Public Class Form1
+
+    Dim draww As Boolean
+    Dim mybrush As New SolidBrush(Color.Black)
+    Dim thick As Integer = 20
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Location = New Point(0, 0)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = MousePosition.X
+        Label2.Text = MousePosition.Y
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        draw.FillEllipse(Brushes.Snow, 100, 200, 230, 230)
+        draw.FillEllipse(Brushes.Snow, 95, 370, 275, 275)
+        draw.FillEllipse(Brushes.Snow, 156, 90, 125, 125)
+        draw.FillRectangle(Brushes.Black, 175, 20, 80, 80)
+        draw.FillEllipse(Brushes.Black, 190, 300, 20, 20)
+        draw.FillEllipse(Brushes.Black, 190, 350, 20, 20)
+        draw.FillEllipse(Brushes.Black, 190, 250, 20, 20)
+        draw.FillEllipse(Brushes.Black, 191, 138, 20, 20)
+        draw.FillEllipse(Brushes.Black, 250, 138, 20, 20)
+        draw.FillEllipse(Brushes.Orange, 229, 150, 20, 20)
+        draw.FillRectangle(Brushes.Black, 161, 100, 110, 20)
+
+
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        If draww = True Then
+            draw.FillEllipse(mybrush, MousePosition.X - 10, MousePosition.Y - 30, thick, thick)
+
+        End If
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        draww = True
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        draww = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        mybrush.Color = (Color.Aquamarine)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mybrush.Color = (Color.Black)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        mybrush.Color = (Color.AliceBlue)
+    End Sub
+
+    Private Sub Red_Click(sender As Object, e As EventArgs) Handles Red.Click
+        mybrush.Color = (Color.Red)
+    End Sub
+
+    Private Sub Orange_Click(sender As Object, e As EventArgs) Handles Orange.Click
+        mybrush.Color = (Color.Orange)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        mybrush.Color = (Color.Yellow)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        mybrush.Color = (Color.ForestGreen)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        mybrush.Color = (Color.Blue)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        mybrush.Color = (Color.BlueViolet)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        mybrush.Color = (Color.DeepPink)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        mybrush.Color = (Color.LightCyan)
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        mybrush.Color = (Color.Lavender)
+    End Sub
+
+
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        mybrush.Color = (Color.Tan)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        mybrush.Color = (Color.Teal)
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim draw As System.Drawing.Graphics
+        draw = Me.CreateGraphics
+        draw.FillRectangle(Brushes.White, 0, 0, 1599, 899)
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        mybrush.Color = (Color.White)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        mybrush.Color = (Color.SaddleBrown)
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        thick = thick + 25
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        thick = thick - 25
+        If thick < 0 Then
+            thick = 5
+        End If
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        thick = thick + 100
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            thick = TextBox1.Text
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Private Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+        If TextBox1.Text = "Insert Text Size" Then
+            TextBox1.Text = ""
+        End If
+    End Sub
+End Class
